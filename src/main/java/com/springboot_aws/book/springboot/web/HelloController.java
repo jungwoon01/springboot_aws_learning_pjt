503c89b5ff8b33d9c516ebeb90d8b88416c67212
jungwoon <jungwoon018@gmail.com>
2021-06-12 16:32:30 +0900

    Update HelloController With @RequestParam

--- src/main/java/com/springboot_aws/book/springboot/web/HelloController.java
+++ src/main/java/com/springboot_aws/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.springboot_aws.book.springboot.web;
 
+import com.springboot_aws.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    // lombok RequestParam 이 있는 Get API
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
