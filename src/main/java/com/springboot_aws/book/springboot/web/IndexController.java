434abe1949bc6c449c5a11072b57527aa6e8fe6e
jungwoon <jungwoon018@gmail.com>
2021-06-14 22:54:07 +0900

    Add all posts page

--- src/main/java/com/springboot_aws/book/springboot/web/IndexController.java
+++ src/main/java/com/springboot_aws/book/springboot/web/IndexController.java
@@ -1,13 +1,22 @@
 package com.springboot_aws.book.springboot.web;
 
+import com.springboot_aws.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private  final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
+        // 여기서는 posts 라는 이름으로 index.mustache 에 전달한다.
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
